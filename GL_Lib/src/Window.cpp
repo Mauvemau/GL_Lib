902a1c8b49df6a4c930177a289b0b54840349d9c
Mau <mauvepinktree@gmail.com>
2024-08-11 19:15:01 -0300

    update window

--- GL_Lib/src/Window.cpp
+++ GL_Lib/src/Window.cpp
@@ -4,6 +4,9 @@ using namespace GLlib;
 using namespace std;
 
 Window::Window(int width, int height, string title) {
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
 	cout << "Window created!\n";
 }
@@ -14,6 +17,24 @@ Window::~Window() {
 	cout << "Window closed.\n";
 }
 
+// Public
+
+void Window::makeContextCurrent() {
+	glfwMakeContextCurrent(window);
+}
+
+void Window::swapBuffers() {
+	glfwSwapBuffers(window);
+}
+
 GLFWwindow* Window::getReference() {
 	return window;
+}
+
+bool Window::getIsInitialized() {
+	return (window);
+}
+
+bool Window::getShouldClose() {
+	return glfwWindowShouldClose(window);
 }
--- GL_Lib/src/main.cpp
+++ GL_Lib/src/main.cpp
@@ -1,40 +1,52 @@
-#include <GLFW/glfw3.h>
+#include "window.h"
+#include "renderer.h"
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    cout << "Hello lib\n";
-    GLFWwindow* window;
-
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
+        cout << "Failed to initialize glfw!\n";
         return -1;
+    }
+    else {
+        cout << "GLFW initialized successfully!\n";
+    }
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    GLlib::Window window(640, 480, "Hello Triangle");
+
+    /* Confirm that the window has been properly initialized */
+    if (!window.getIsInitialized()) {
+        cout << "Failed to create window!\n";
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    window.makeContextCurrent();
 
+    /* Check if the PC has a working OpenGL driver */
+    cout << glGetString(GL_VERSION) << "\n";
+
+    cout << "Render loop has started!\n";
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!window.getShouldClose())
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        window.swapBuffers();
 
         /* Poll for and process events */
         glfwPollEvents();
     }
+    cout << "Render loop ended\n";
 
+    /* Terminate the glfw context and window */
     glfwTerminate();
 
 	return 0;
