cb6e7f7ccea08cca07217ae7b5e6d732837c4430
Mau <mauvepinktree@gmail.com>
2024-08-13 11:50:43 -0300

    add comment on core

--- GL_Lib/src/core.h
+++ GL_Lib/src/core.h
@@ -11,6 +11,10 @@ namespace gllib {
 		/// Can only be called after initializing a window and setting up the glfw context
 		/// </summary>
 		static bool initGlad();
+
+		/// <summary>
+		/// Must be called on every frame to poll glfw and other library utils
+		/// </summary>
 		static void pollEvents();
 	};
 
