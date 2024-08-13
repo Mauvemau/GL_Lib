6460c4c101cb6d946a1beaa8d3359df15f0391c1
Mau <mauvepinktree@gmail.com>
2024-08-13 09:50:05 -0300

    update project filters

--- GL_Lib/src/window.cpp
+++ GL_Lib/src/window.cpp
@@ -4,9 +4,6 @@ using namespace gllib;
 using namespace std;
 
 Window::Window(int width, int height, string title) {
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
 	cout << "Window created!\n";
 }
