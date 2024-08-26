b01f7916f99efec0eff540d0217a5dec9c532a07
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2024-08-26 15:55:47 -0300

    add entity and transform

---
+++ GL_Lib/src/entity.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include "transform.h"
+#include "deps.h"
+
+namespace gllib {
+    
+    class DLLExport Entity {
+        
+    protected:
+        Transform transform;
+
+    public:
+        Entity(const Transform& transform);
+        virtual ~Entity();
+
+        void move(const Vector3& position) const;
+        void rotate(const Quaternion& rotation) const;
+        void updateTransform();
+
+        Vector3 upward() const;
+        Vector3 forward() const;
+        Vector3 right() const;
+        
+        Transform getTransform() const;
+        Vector3 getPosition() const;
+        Vector3 getScale() const;
+        Vector3 getRotationEuler() const;
+        Quaternion getRotationQuat() const;
+
+        void setTransform(const Transform& transform);
+        void setPosition(const Vector3& position);
+        void setScale(const Vector3& scale);
+        void setRotationQuat(const Quaternion& rotation);
+        void setRotationEuler(const Vector3& rotation);
+        void setColor(const Color& color);
+        bool isColliding(const Transform& _transform) const;
+        bool isColliding(float x, float y, float width, float height) const;
+
+    };
+}
