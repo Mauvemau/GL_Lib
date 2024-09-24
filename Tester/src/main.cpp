e4a2c70d8a53f6f0ff8a9b00bce3b37da174ab32
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2024-09-24 12:37:09 -0300

    Update main.cpp

--- Tester/src/main.cpp
+++ Tester/src/main.cpp
@@ -6,13 +6,7 @@ using namespace std;
 
 class Game : public gllib::BaseGame {
 private:
-    gllib::Triangle* triangle;
     gllib::Rectangle* rectangle;
-
-    bool movingRight;
-    float color = 0;
-
-    void moveRectangle(float speed);
 
 protected:
     void init() override;
@@ -27,19 +21,12 @@ public:
 Game::Game() {
     window->setVsyncEnabled(true);
     cout << "Game created!\n";
+
     gllib::Transform trs;
-    trs.position = { 100.0f, 100.0f, 0.0f };
+    trs.position = { 0.0f, 200.0f, 0.0f };
     trs.rotationQuat = { 0.0f, 0.0f, 0.0f, 0.0f };
-    trs.scale = { 57.74f, 50.0f, 0.0f };
-    triangle = new gllib::Triangle(trs, { 0.85f, 0.2f, 0.4f, 1.0f });
-
-    gllib::Transform trs2;
-    trs2.position = { 200.0f, 200.0f, 0.0f };
-    trs2.rotationQuat = { 0.0f, 0.0f, 0.0f, 0.0f };
-    trs2.scale = { 100.0f, 100.0f, 0.0f };
-    rectangle = new gllib::Rectangle(trs2, { 1.0f, 1.0f, 1.0f, 1.0f });
-
-    movingRight = false;
+    trs.scale = { 1000.0f, 1000.0f, 0.0f };
+    rectangle = new gllib::Rectangle(trs, { 0.0f, 0.0f, 1.0f, 1.0f });
 }
 
 Game::~Game() {
@@ -54,50 +41,22 @@ void Game::init() {
 }
 
 void Game::update() {
-    // Update
-    gllib::Quaternion rot = triangle->getRotationQuat();
-    rot.z += gllib::LibTime::getDeltaTime() * 30.0f;
-    triangle->setRotationQuat(rot);
-
+    
     rectangle->rotate({ 0.0f, 0.0f, static_cast<float>(gllib::LibTime::getDeltaTime() * 100.0f)});
-    color += 0.5 * gllib::LibTime::getDeltaTime();
-    if (color > 1.0f) color = 0.0f;
-    rectangle->setColor({ color, color, 1, 1.0f });
-
-    moveRectangle(50);
-
-    // Draw
+    gllib::Vector3 direction = rectangle->getPosition();
+    direction.x += 1.0f;
+    rectangle->setPosition(direction);
+    
+    rectangle->setScale(rectangle->getScale() * 0.99f);
+    
     gllib::Renderer::clear();
 
-    triangle->draw();
     rectangle->draw();
-}
-
-void Game::moveRectangle(float speed) {
-    gllib::Vector3 position = rectangle->getPosition();
-    if (movingRight)
-    {
-        position.x += gllib::LibTime::getDeltaTime() * speed; // Adjust speed as needed
-        if (position.x >= window->getWidth() - 100.0f)
-        {
-            movingRight = false;
-        }
-    }
-    else
-    {
-        position.x -= gllib::LibTime::getDeltaTime() * speed; // Adjust speed as needed
-        if (position.x <= 100.0f)
-        {
-            movingRight = true;
-        }
-    }
-    rectangle->setPosition(position);
 }
 
 
 void Game::uninit() {
     cout << "External uninit!!!\n";
-    delete triangle;
     delete rectangle;
 }
 
