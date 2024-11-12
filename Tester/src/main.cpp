ed1f32a4ea7dffebc16281307c2ac60fa959e706
mau <mauvepinktree@gmail.com>
2024-11-12 14:40:02 -0300

    add parcial

--- Tester/src/main.cpp
+++ Tester/src/main.cpp
@@ -1,28 +1,37 @@
 #include "base_game.h"
 
 #include <iostream>
+#include <map>
 
 #include "collisionManager.h"
 
 using namespace std;
 
+enum class CharacterStatus {
+    idle,
+    attack,
+    dying
+};
+
 class Game : public gllib::BaseGame {
 private:
-    gllib::Triangle* triangle;
-    gllib::Sprite* sprite;
-    gllib::Sprite* background;
-    gllib::Animation* coin;
-    gllib::Animation* player;
-    gllib::Rectangle* floorCollision;
-    gllib::collisionManager* collisionManager;
-    float animSpeed, nextFrame;
+    gllib::Animation* erikaAttack;
+    gllib::Animation* erikaDeath;
+    bool erikaAttacking;
+    bool erikaDead;
+    gllib::Animation* hectorAttack;
+    gllib::Animation* hectorDeath;
+    bool hectorAttacking;
+    bool hectorDead;
 
-    void moveRectangle(float speed);
-    void movement(gllib::Animation* player);
+    bool faded;
+    float nextFade;
+    float fadeInterval;
+
+    void handleInput();
 
 protected:
     void init() override;
-    void drawObjects();
     void update() override;
     void uninit() override;
 
@@ -31,73 +40,9 @@ public:
     ~Game() override;
 };
 
-Game::Game()
-{
+Game::Game() {
     window->setVsyncEnabled(false);
     cout << "Game created!\n";
-
-    gllib::Transform trs;
-    trs.position = { 100.0f, 100.0f, 0.0f };
-    trs.rotationQuat = { 0.0f, 0.0f, 0.0f, 0.0f };
-    trs.scale = { 57.74f, 50.0f, 0.0f };
-    triangle = new gllib::Triangle(trs, { 0.85f, 0.2f, 0.4f, 1.0f });
-    
-    gllib::Transform trs2;
-    trs2.position = { 400.0f, 400.0f, 0.0f };
-    trs2.rotationQuat = { 0.0f, 0.0f, 0.0f, 0.0f };
-    trs2.scale = { 100.0f, 100.0f, 0.0f };
-    sprite = new gllib::Sprite(trs2, { 1.0f, 1.0f, 1.0f, 1.0f });
-    coin = new gllib::Animation(trs2, { 1.0f, 1.0f, 1.0f, 1.0f });
-    trs2.position = { window->getWidth() * .5f, window->getHeight() * .5f, 0.0f };
-    player = new gllib::Animation(trs2, { 1.0f, 1.0f, 1.0f, 1.0f });
-
-    gllib::Transform trs3;
-    trs3.position = { window->getWidth() * .5f, window->getHeight() * .5f, 0.0f };
-    trs3.rotationQuat = { 0.0f, 0.0f, 0.0f, 0.0f };
-    trs3.scale = { 640.0f, 480.0f, 0.0f };
-    background = new gllib::Sprite(trs3, { 1.0f, 1.0f, 1.0f, 1.0f });
-    background->addTexture("background.png", true);
-
-    gllib::Transform trs4;
-    trs4.position = {window->getWidth() * .5f, window->getHeight() * .95f, 0};
-    trs4.rotationQuat = { 0.0f, 0.0f, 0.0f, 0.0f };
-    trs4.scale = {static_cast<float>(window->getWidth()), 80, 0};
-    floorCollision = new gllib::Rectangle(trs4, {0.8f, 0.0f, 1.0f, 0.5f});
-
-    collisionManager = new gllib::collisionManager({static_cast<gllib::Entity*>(floorCollision)});
-    
-    sprite->addTexture("sus.png", true);
-    sprite->setMirroredX(true);
-    int textureWidth = 16;
-    unsigned int coinTex = gllib::Loader::loadTexture("coin.png", true);
-    coin->addFrames(coinTex, textureWidth, 16, 8, 1);
-    coin->setCurrentFrame(7);
-
-    unsigned int sonicTex = gllib::Loader::loadTexture("Sonic_Atlas.png", true);
-    player->addFrame(sonicTex, 273, 118, 33, 41);
-    player->addFrame(sonicTex, 305, 118, 33, 41);
-    player->addFrame(sonicTex, 340, 118, 35, 41);
-    player->addFrame(sonicTex, 379, 118, 38, 41);
-    player->addFrame(sonicTex, 418, 118, 36, 41);
-    player->addFrame(sonicTex, 456, 118, 32, 41);
-    player->addFrame(sonicTex, 488, 118, 32, 41);
-    player->addFrame(sonicTex, 522, 118, 32, 41);
-    player->addFrame(sonicTex, 558, 118, 34, 41);
-    player->addFrame(sonicTex, 595, 118, 37, 41);
-    player->addFrame(sonicTex, 636, 118, 34, 41);
-    player->addFrame(sonicTex, 673, 118, 32, 41);
-
-    //player->addFramesFromAtlas(sonicTex, 277, 118, 35, 40, 12, 1);
-
-    player->setCurrentFrame(0);
-    player->setDurationInSecs(1.f);
-    
-    coin->setCurrentFrame(0);
-
-    coin->setDurationInSecs(.6);
-
-    animSpeed = .075f;
-    nextFrame = 0;
 }
 
 Game::~Game() {
@@ -105,152 +50,154 @@ Game::~Game() {
 }
 
 void Game::init() {
-    cout << "External init!!!!\n";
-
     srand(time(nullptr));
-    window->setTitle("Example game lmao");
+    window->setTitle("Parcial 2 Salazar");
+    gllib::Transform trs1;
+    trs1.position = { 200.0f, 200.0f, 0.0f };
+    trs1.rotationQuat = { 0.0f, 0.0f, 0.0f, 0.0f };
+    trs1.scale = { 100.0f, 100.0f, 0.0f };
+    erikaAttack = new gllib::Animation(trs1, { 1.0f, 1.0f, 1.0f, 1.0f });
+    unsigned int erikaTex = gllib::Loader::loadTexture("Eirika_Lord_Sword_Sprite_Sheet.gif", true);
+    erikaAttack->addFrame(erikaTex, 3, 6, 36, 34);
+    erikaAttack->addFrame(erikaTex, 42, 6, 37, 34);
+    erikaAttack->addFrame(erikaTex, 85, 3, 29, 40);
+    erikaAttack->addFrame(erikaTex, 123, 3, 28, 41);
+    erikaAttack->addFrame(erikaTex, 157, 3, 28, 41);
+    erikaAttack->addFrame(erikaTex, 193, 7, 28, 33);
+    erikaAttack->addFrame(erikaTex, 229, 7, 28, 31);
+    erikaAttack->addFrame(erikaTex, 264, 6, 29, 31);
+    erikaAttack->addFrame(erikaTex, 296, 3, 62, 30);
+    erikaAttack->addFrame(erikaTex, 361, 6, 54, 27);
+    erikaAttack->addFrame(erikaTex, 421, 6, 54, 27);
+    erikaAttack->addFrame(erikaTex, 3, 52, 53, 28);
+    erikaAttack->addFrame(erikaTex, 59, 51, 52, 29);
+    erikaAttack->addFrame(erikaTex, 114, 48, 42, 32);
+    erikaAttack->addFrame(erikaTex, 161, 48, 33, 34);
+    erikaAttack->addFrame(erikaTex, 207, 45, 20, 32);
+    erikaAttack->addFrame(erikaTex, 242, 45, 20, 32);
+    erikaAttack->addFrame(erikaTex, 273, 47, 36, 33);
+    erikaAttack->setDurationInSecs(1.5f);
+    erikaAttack->reset();
+    erikaAttack->setMirroredX(true);
+    erikaDeath = new gllib::Animation(trs1, { 1.0f, 1.0f, 1.0f, 1.0f });
+    erikaDeath->addFrame(erikaTex, 380, 138, 36, 35);
+    erikaDeath->addFrame(erikaTex, 420, 137, 28, 36);
+    erikaDeath->addFrame(erikaTex, 454, 137, 28, 36);
+    erikaDeath->setDurationInSecs(.2f);
+    erikaDeath->reset();
+    erikaDeath->setMirroredX(true);
+    erikaDead = false;
+    erikaAttacking = false;
+
+    gllib::Transform trs2;
+    trs2.position = { 400.0f, 200.0f, 0.0f };
+    trs2.rotationQuat = { 0.0f, 0.0f, 0.0f, 0.0f };
+    trs2.scale = { 100.0f, 100.0f, 0.0f };
+    hectorAttack = new gllib::Animation(trs2, { 1.0f, 1.0f, 1.0f, 1.0f });
+    unsigned int hectorTex = gllib::Loader::loadTexture("Hector_Lord_Sprite_Sheet.gif", true);
+    hectorAttack->addFrame(hectorTex, 7, 19, 39, 36);
+    hectorAttack->addFrame(hectorTex, 49, 19, 30, 36);
+    hectorAttack->addFrame(hectorTex, 80, 20, 27, 35);
+    hectorAttack->addFrame(hectorTex, 108, 20, 27, 35);
+    hectorAttack->addFrame(hectorTex, 138, 20, 27, 35);
+    hectorAttack->addFrame(hectorTex, 167, 0, 45, 55);
+    hectorAttack->addFrame(hectorTex, 219, 0, 59, 55);
+    hectorAttack->addFrame(hectorTex, 6, 90, 45, 27);
+    hectorAttack->addFrame(hectorTex, 54, 91, 45, 27);
+    hectorAttack->addFrame(hectorTex, 103, 91, 45, 27);
+    hectorAttack->addFrame(hectorTex, 158, 76, 34, 43);
+    hectorAttack->addFrame(hectorTex, 196, 61, 29, 46);
+    hectorAttack->addFrame(hectorTex, 235, 61, 29, 46);
+    hectorAttack->addFrame(hectorTex, 6, 126, 41, 48);
+    hectorAttack->addFrame(hectorTex, 49, 127, 42, 47);
+    hectorAttack->addFrame(hectorTex, 95, 134, 45, 40);
+    hectorAttack->addFrame(hectorTex, 144, 143, 47, 31);
+    hectorAttack->addFrame(hectorTex, 195, 143, 46, 31);
+    hectorAttack->addFrame(hectorTex, 246, 143, 41, 31);
+    hectorAttack->addFrame(hectorTex, 7, 190, 54, 32);
+    hectorAttack->addFrame(hectorTex, 78, 192, 47, 30);
+    hectorAttack->addFrame(hectorTex, 127, 190, 44, 32);
+    hectorAttack->addFrame(hectorTex, 175, 190, 28, 32);
+    hectorAttack->setDurationInSecs(2.0f);
+    hectorAttack->reset();
+    hectorDeath = new gllib::Animation(trs2, { 1.0f, 1.0f, 1.0f, 1.0f });
+    hectorDeath->addFrame(hectorTex, 185, 307, 54, 40);
+    hectorDeath->addFrame(hectorTex, 6, 126, 41, 48);
+    hectorDeath->addFrame(hectorTex, 48, 352, 55, 36);
+    hectorDeath->addFrame(hectorTex, 242, 391, 43, 36);
+    hectorDeath->setDurationInSecs(.3);
+    hectorDeath->reset();
+    hectorDead = false;
+    hectorAttacking = false;
+
+    faded = false;
+    nextFade = .0f;
+    fadeInterval = .15f;
 }
 
 
 void Game::update() {
     // Update
-    movement(player);
+    handleInput();
+    erikaDeath->update();
+    hectorDeath->update();
 
-    coin->update();
-    player->update();
-
-    gllib::Quaternion rot = triangle->getRotationQuat();
-    rot.z += gllib::LibTime::getDeltaTime() * 30.0f;
-    triangle->setRotationQuat(rot);
-
-    moveRectangle(100);
+    if (nextFade < gllib::LibTime::getElapsedTime()) {
+        nextFade = gllib::LibTime::getElapsedTime() + fadeInterval;
+        faded = !faded;
+    }
 
     // Draw
-    
-    
-    drawObjects();
-}
-
-
-void Game::drawObjects()
-{
     gllib::Renderer::clear();
 
     gllib::Shader::useShaderProgram(shaderProgramTexture);
-    background->draw();
-    sprite->draw();
-    coin->draw();
-    player->draw();
+    // draw textures
+    if (!erikaDead) {
+        erikaAttack->draw();
+    }
+    else {
+        if (faded) {
+            erikaDeath->draw();
+        }
+    }
+    if (!hectorDead) {
+        hectorAttack->draw();
+    }
+    else {
+        if (faded) {
+            hectorDeath->draw();
+        }
+    }
 
     gllib::Shader::useShaderProgram(shaderProgramSolidColor);
-    triangle->draw();
-}
-
-static int x = 1;
-static int y = 1;
-
-void Game::moveRectangle(float speed) {
-    if (sprite->getPosition().x - (sprite->getScale().x * .5) <= 0) {
-        x = 1;
-        sprite->setMirroredX(true);
-    }
-    if (sprite->getPosition().x + (sprite->getScale().x * .5) >= window->getWidth()) {
-        x = -1;
-        sprite->setMirroredX(false);
-        gllib::Vector3 scale = coin->getScale();
-    }
-
-    if (sprite->getPosition().y - (sprite->getScale().y * .5) <= 0) {
-        y = 1;
-        sprite->setMirroredY(true);
-    }
-    if (sprite->getPosition().y + (sprite->getScale().y * .5) >= window->getHeight()) {
-        y = -1;
-        sprite->setMirroredY(false);
-    }
-    
-    gllib::Vector3 scale = coin->getScale();
-    scale.x += (25.0f * gllib::LibTime::getDeltaTime()) * x;
-    scale.y += (25.0f * gllib::LibTime::getDeltaTime()) * x;
-    coin->setScale(scale);
-
-    sprite->move({static_cast<float>(x * (speed * gllib::LibTime::getDeltaTime())), 
-                  static_cast<float>(y * (speed * gllib::LibTime::getDeltaTime())), 
-                  0.0f});
-    //sprite->rotate({ 0.0f, 0.0f, static_cast<float>(gllib::LibTime::getDeltaTime() * -60.0f) });
-}
-
-void Game::movement(gllib::Animation* player)
-{
-    gllib::Transform transform2 = player->getTransform();
-    transform2.position.y += 1.f;
-    float speed = 80 * gllib::LibTime::getDeltaTime();
-    float gravity = 40 * gllib::LibTime::getDeltaTime();
-    if (!collisionManager->checkCollision(transform2))
-    {
-        player->move({0.f, gravity, 0});
-    }
-
-    if (Input::getKeyReleased(Key_R)) {
-        player->setAnimationPaused(true);
-        player->reset();
-    }
-
-    if (Input::getKeyPressed(Key_Q)) {
-        player->setAnimationPaused(false);
-    }
-
-    if (!Input::isAnyKeyPressed()) return;
-    gllib::Transform transform = player->getTransform();
-
-    if (Input::getKeyPressed(Key_D))
-    {
-        // D
-        transform.position.x += 2.0f;
-        player->setMirroredX(false);
-        if (!collisionManager->checkCollision(transform))
-        {
-            player->move({speed, 0.f, 0.f});
-        }
-    }
-
-    if (Input::getKeyPressed(Key_A))
-    {
-        // A
-        transform.position.x -= 2.0f;
-        player->setMirroredX(true);
-        if (!collisionManager->checkCollision(transform))
-        {
-            player->move({-speed, 0.f, 0.f});
-        }
-    }
-    
-    if (Input::getKeyPressed(Key_W))
-    {
-        // W
-        transform.position.y -= 2.0f;
-        if (!collisionManager->checkCollision(transform))
-        {
-            player->move({0.f, -speed, 0.f});
-        }
-    }
-
-    if (Input::getKeyPressed(Key_S))
-    {
-        // S
-        transform.position.y += 2.0f;
-        if (!collisionManager->checkCollision(transform))
-        {
-            player->move({0.f, speed, 0.f});
-        }
-    }
+    // draw shapes
 }
 
 void Game::uninit() {
-    cout << "External uninit!!!\n";
-    delete triangle;
-    delete sprite;
-    delete coin;
+    delete erikaAttack;
+    delete erikaDeath;
+    delete hectorAttack;
+    delete hectorDeath;
+}
+
+void Game::handleInput() {
+    if (Input::getKeyPressed(Key_Q)) {
+        erikaAttack->update();
+        hectorDead = true;
+    }
+    if (Input::getKeyReleased(Key_Q)) {
+        erikaAttack->reset();
+        hectorDead = false;
+    }
+
+    if (Input::getKeyPressed(Key_E)) {
+        hectorAttack->update();
+        erikaDead = true;
+    }
+    if (Input::getKeyReleased(Key_E)) {
+        hectorAttack->reset();
+        erikaDead = false;
+    }
 }
 
 int main() {
